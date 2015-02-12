55a3e466944d2fcba3a60f85a1e095817b53fe22
tessbianchi <TessBianchi@Tesss-MacBook-Pro-2.local>
2015-02-12 13:30:36 -0500

    Map Subsystem

--- src/main/java/com/oopsididitagain/model/Tile.java
+++ src/main/java/com/oopsididitagain/model/Tile.java
@@ -1,5 +1,59 @@
 package com.oopsididitagain.model;
 
+import java.util.*;
+
 public class Tile {
-test
+	private Entity entity;
+	private List<Item> items;
+	private Terrain t;
+	private AreaEffect a;
+	private Decal d;
+	
+	
+	public Decal getD() {
+		return d;
+	}
+
+
+	public Tile(Terrain t) {
+		super();
+		this.t = t;
+	}
+
+	
+	public void setD(Decal d) {
+		this.d = d;
+	}
+
+
+	public Terrain getT() {
+		return t;
+	}
+
+	public void setT(Terrain t) {
+		this.t = t;
+	}
+
+	public AreaEffect getA() {
+		return a;
+	}
+
+	public void setA(AreaEffect a) {
+		this.a = a;
+	}
+	
+	public Entity getEntity() {
+		return entity;
+	}
+	public void setEntity(Entity entity) {
+		this.entity = entity;
+	}
+	public List<Item> getItems() {
+		return items;
+	}
+	public void setItems(List<Item> items) {
+		//this.items = items;
+	}
+
+	
 }
