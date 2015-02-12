8758ba02c7eecc26988137b18f638645f0880cea
Joseph Liccini <joseph.liccini@ufl.edu>
2015-02-11 23:17:29 -0500

    Added a Test Image, and displayed initial GUI Elements.

---
+++ src/com/oopsididitagain/GamePanel.java
@@ -0,0 +1,25 @@
+package com.oopsididitagain;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+
+	private static final long serialVersionUID = 8740227504423945127L;
+	
+	private void showImage(Graphics g) {
+		Image image = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/Zelda-Light-World.jpg"));
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		showImage(g);
+	}
+
+
+}
---
+++ src/com/oopsididitagain/Main.java
@@ -0,0 +1,35 @@
+package com.oopsididitagain;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import net.miginfocom.swing.MigLayout;
+
+public class Main extends JFrame {
+
+	private static final long serialVersionUID = -4222070071039252746L;
+	
+	public static void main(String[] args) {
+	    SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				Main main = new Main();
+				main.initialize();
+			}
+	    });
+	}
+	
+	private void initialize() {
+		this.setLayout(new MigLayout("", "[grow, fill]", "[grow, fill]"));
+		
+		JPanel mainPanel = new GamePanel();
+		this.add(mainPanel);
+
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(600, 600);
+		this.setLocationRelativeTo(null); // places frame into center of screen
+		this.setVisible(true);
+	}
+	
+}
