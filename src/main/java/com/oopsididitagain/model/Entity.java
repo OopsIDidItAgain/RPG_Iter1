0eecc8aa153563f732353578f8e6d1645db666e7
Pablo Arango <pabloaran17@gmail.com>
2015-02-12 13:51:09 -0500

    The Inventory is IN DA HAUS

--- src/main/java/com/oopsididitagain/model/Entity.java
+++ src/main/java/com/oopsididitagain/model/Entity.java
@@ -2,4 +2,5 @@ package com.oopsididitagain.model;
 
 public class Entity extends GameObject {
 	private static final long serialVersionUID = 6897867058695115274L;
+    private Inventory inventory;
 }
