1a856725ec51afa0013dc9de5c9c36b02a699565
Joseph Liccini <joseph.liccini@ufl.edu>
2015-02-12 17:02:05 -0500

    Change Map to GameMap

--- src/main/java/com/oopsididitagain/model/GameMap.java
+++ src/main/java/com/oopsididitagain/model/GameMap.java
@@ -1,5 +1,23 @@
 package com.oopsididitagain.model;
 
 public class GameMap {
+	Tile [][] tiles;
 
+	public GameMap(Tile[][] tiles) {
+		super();
+		this.tiles = tiles;
+	}
+
+	public Tile[][] getTiles() {
+		return tiles;
+	}
+
+	public void setTiles(Tile[][] tiles) {
+		this.tiles = tiles;
+	}
+	
+	public Tile getATile(int x, int y){
+		
+		return tiles[x][y];
+	}
 }
