cada29b03a6ca1c67ea8f07951f238c27ba9fa6a
Pablo Arango <pabloaran17@gmail.com>
2015-02-12 13:37:16 -0500

    intelliJ stuff

--- src/main/java/com/oopsididitagain/model/Entity.java
+++ src/main/java/com/oopsididitagain/model/Entity.java
@@ -1,8 +1,5 @@
 package com.oopsididitagain.model;
 
-import com.oopsididitagain.model.GameObject;
-
 public class Entity extends GameObject {
 	private static final long serialVersionUID = 6897867058695115274L;
-
 }
