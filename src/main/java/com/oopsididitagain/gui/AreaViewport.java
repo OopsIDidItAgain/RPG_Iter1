b2d3d1b40ecbe964ba4cfffb574556afa62075f9
tessbianchi <TessBianchi@Tesss-MacBook-Pro-2.local>
2015-02-12 17:56:13 -0500

    Views stuffz

--- src/main/java/com/oopsididitagain/gui/AreaViewport.java
+++ src/main/java/com/oopsididitagain/gui/AreaViewport.java
@@ -1,5 +1,55 @@
 package com.oopsididitagain.gui;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import com.oopsididitagain.model.*;
+
 
 public class AreaViewport extends Viewport{
+	private GameMap map;
+	private Entity avatar;
+	public static final int TILE_SIZE = 20;//in pixels
+	
+	public AreaViewport(GameMap map, Entity avatar) {
+		super();
+		this.map = map;
+		this.avatar = avatar;
+	}
+	
+	
+	
+	public GameMap getMap() {
+		return map;
+	}
+
+	public void setMap(GameMap map) {
+		this.map = map;
+	}
+
+	public Entity getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(Entity avatar) {
+		this.avatar = avatar;
+	}
+
+/*
+
+	public int getxCoord1(){
+		int x = avatar.getPos().getX();
+		
+	}
+	public int getxCoord2(){
+		
+	}	
+	public int getyCoord(){
+		
+	}	
+
+*/
+	
+	
 
 }
--- src/main/java/com/oopsididitagain/gui/Main.java
+++ src/main/java/com/oopsididitagain/gui/Main.java
@@ -1,5 +1,7 @@
 package com.oopsididitagain.gui;
 
+import com.oopsididitagain.model.*;
+import com.oopsididitagain.util.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
@@ -22,11 +24,25 @@ public class Main extends JFrame {
 	
 	private void initialize() {
 		this.setLayout(new MigLayout("", "[grow, fill]", "[grow, fill]"));
-		AreaViewport areaViewport = new AreaViewport();
+		
+		Entity avatar = new Entity();
+		Position p = new Position(0,0);
+		avatar.setPos(p);
+		Tile [][] t = new Tile[2][2];
+		Terrain l = new Terrain(0);
+		Terrain m = new Terrain(1);
+		t[0][0] = new Tile(l);
+		t[0][1] = new Tile(m);
+		t[1][0] = new Tile(l);
+		t[1][1] = new Tile(m);
+		
+		GameMap gameMap = new GameMap(t);
+		AreaViewport areaViewport = new AreaViewport(gameMap,avatar);
+		
+		
 		StatsViewport statsViewport = new StatsViewport();
 		JPanel mainPanel = new View(areaViewport,statsViewport);
 		this.add(mainPanel);
-
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(600, 600);
 		this.setLocationRelativeTo(null); // places frame into center of screen
