c6dc62c604d97c88ab94c07c6881dd065833e043
dannysepler <dsepler@ufl.edu>
2015-02-13 15:49:39 -0500

    Added simple things to StatsViewport

--- src/main/java/com/oopsididitagain/gui/StatsViewport.java
+++ src/main/java/com/oopsididitagain/gui/StatsViewport.java
@@ -1,5 +1,35 @@
 package com.oopsididitagain.gui;
 
-public class StatsViewport extends Viewport{
+public class StatsViewport extends Viewport {
+	private static final long serialVersionUID = -4960198915890489235L;
+	// Stats stats; // this doesn't exist in code yet
+	
+	public StatsViewport() {
+		super();
+		setFocusable(true);
+	}
+	
+	/* 		// these all incorporate a "Stats" object,
+			// which doesn't exist in code yet
+			 
+	public StatsViewport(Stats stats) {
+		this.stats = stats;
+		
+		super();
+		setFocusable(true);
+	}	
+	
+	
+	public Stats getStats() {
+		return stats;
+	}
 
+	public void setStats(Stats stats) {
+		this.stats = stats;
+	}
+	
+	
+	
+	*/
 }
+
