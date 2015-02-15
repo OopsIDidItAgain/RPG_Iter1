88e897d88b038ba3d6f625fe32d2126c8f308da9
oepstein <orreo140@ufl.edu>
2015-02-15 12:05:26 -0500

    Continuation of Item implementation

--- src/main/java/com/oopsididitagain/model/Item.java
+++ src/main/java/com/oopsididitagain/model/Item.java
@@ -1,12 +1,22 @@
 package com.oopsididitagain.model;
 
+import java.awt.Image;
 
 public abstract class Item extends GameObject {
 	private static final long serialVersionUID = 4933860466914150600L;
 	
-	private String name;
-	
 	public Item(String name) {
 		this.name = name;
+	}
+	
+	public Item(String name, Image image) {
+		this.name = name;
+		this.image = image;
+	}
+	
+	public Item(String name, Image image, Position position) {
+		this.name = name;
+		this.image = image;
+		this.position = position;
 	}
 }
--- src/main/java/com/oopsididitagain/model/TakeableItem.java
+++ src/main/java/com/oopsididitagain/model/TakeableItem.java
@@ -1,14 +1,17 @@
 package com.oopsididitagain.model;
 
+import java.awt.Image;
+
 /* Should this be an interface maybe? Takeable? */
 public class TakeableItem extends Item {
 	private static final long serialVersionUID = -7151959478242233966L;
 	
 	private boolean isEquippable;
 	
-	public TakeableItem(String name, boolean equippable) {
+	public TakeableItem(String name, boolean equippable, Image image) {
 		super(name);
 		isEquippable = equippable;
+		this.image = image;
 	}
 	
 	public boolean isEquippable() {
